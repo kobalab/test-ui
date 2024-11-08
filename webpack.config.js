f1e0f9c3caea381227701e73affec28bc420bf7b
Satoshi Kobayashi <koba@yk.rim.or.jp>
2024-11-08 10:17:38 +0800

    webpack.config.js 初期登録

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const version = require('./package.json').version;
+
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    entry:  {
+        'test-ui': './src/js/test-ui.js',
+    },
+    output: {
+        path:     __dirname + '/dist/js/',
+        filename: `[name]-${version}.js`
+    },
+    optimization: {
+        minimizer: [ new TerserPlugin({extractComments: false}) ],
+    },
+};
