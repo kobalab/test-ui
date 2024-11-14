5d81df9c68d2ba4c6e9b2b244eac1b5525bd5b5a
Satoshi Kobayashi <koba@yk.rim.or.jp>
2024-11-14 15:28:27 +0800

    pai を追加

--- src/js/lib/index.js
+++ src/js/lib/index.js
@@ -1,5 +1,6 @@
 "use strict";
 module.exports = {
+    pai:         require('./pai'),
     Util:        Object.assign(require('./fadein'),
                                require('./selector'),
                                require('./scale'))
---
+++ src/js/lib/label.js
@@ -0,0 +1,23 @@
+/*
+ *  label.js
+ */
+"use strict";
+
+function pai() {
+    const suit  = { m: 'ワン', p: 'ピン', s: 'ソー' };
+    const num   = [ '赤ウー', 'イー', 'リャン', 'サン', 'スー',
+                   'ウー', 'ロー', 'チー', 'パー', 'キュー' ];
+    const zipai = [ 'トン', 'ナン', 'シャー', 'ペー', 'ハク', 'ハツ', 'チュン' ];
+    let rv = {};
+    for (let s of Object.keys(suit)) {
+        for (let n = 0; n <= 9, n++) {
+            if (s != 'z')            rv[s+n] = num[n] + suit[s];
+            else if (0 < n && n < 8) rv[s+n] = zipai[n-1];
+        }
+    }
+    return rv;
+}
+
+module.exports = function(type) {
+    if (type == 'pai') return pai();
+}
