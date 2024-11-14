18c910c9c9f6791fa016dc8bc3efcf668029d971
Satoshi Kobayashi <koba@yk.rim.or.jp>
2024-11-14 16:47:10 +0800

    aria-label を設定する処理を追加

--- src/js/lib/pai.js
+++ src/js/lib/pai.js
@@ -4,13 +4,16 @@
 "use strict";
 
 const $ = require('jquery');
+const _label = require('./label')('pai');
 
-module.exports = function(loaddata) {
+module.exports = function(loaddata, label = _label) {
 
     const pai = {};
 
     $('.pai', loaddata).each((i, n)=>{
         let p = $(n).data('pai');
+        if ($(n).attr('alt') != null) $(n).attr('alt', label[p]);
+        else                          $(n).attr('aria-label', label[p]);
         pai[p] = $(n);
     });
 
