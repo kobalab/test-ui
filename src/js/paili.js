ec78fbd449bc0a5bd413bf2289c7ee12a9adf2e7
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-03-14 19:10:08 +0800

    牌理に手牌を表示する処理を追加

--- src/js/paili.js
+++ src/js/paili.js
@@ -6,3 +6,33 @@
  *  https://github.com/kobalab/Majiang/blob/master/LICENSE
  */
 "use strict";
+
+const model = {};
+const view  = {};
+
+const rule = Majiang.rule();
+
+function qipai() {
+
+    model.shan = new Majiang.Shan(rule);
+
+    let qipai = [];
+    while (qipai.length < 13) qipai.push(model.shan.zimo());
+    model.shoupai = new Majiang.Shoupai(qipai);
+    model.shoupai.zimo(model.shan.zimo());
+
+    let paistr = model.shoupai.toString();
+    $('[name="paistr"]').val(paistr);
+
+    view.shoupai = new Majiang.UI.Shoupai(
+                                $('.shoupai'), view.pai, model.shoupai
+                            ).redraw(true);    
+}
+
+$(function(){
+
+    view.pai   = Majiang.UI.pai('#loaddata');
+    view.audio = Majiang.UI.audio('#loaddata');
+
+    qipai();
+});
