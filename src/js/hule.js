50eadeea5bf7448d25d5f3c22418e4de710ba4c6
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-03-18 06:56:44 +0800

    和了点計算のページを初期登録

---
+++ src/js/hule.js
@@ -0,0 +1,8 @@
+/*!
+ *  電脳麻将: 和了点計算 v0.2.3
+ *
+ *  Copyright(C) 2017 Satoshi Kobayashi
+ *  Released under the MIT license
+ *  https://github.com/kobalab/Majiang/blob/master/LICENSE
+ */
+"use strict";
