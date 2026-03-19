4f021a0af87672b427435c3929a5b4f938851678
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-03-20 07:13:11 +0800

    和了点計算に和了点の表示を追加

--- src/js/hule.js
+++ src/js/hule.js
@@ -7,10 +7,58 @@
  */
 "use strict";
 
-function submit() {
+const { hide, show, fadeIn, fadeOut } = Majiang.UI.Util;
+
+let dialog;
+
+function init() {
+
+    let paistr = 'm123p123z1z1,s1-23,z222=';
+    let baopai = ['z1'];
+
+    $('input[name="paistr"]').val(paistr).focus();
+    for (let i = 0; i < baopai.length; i++) {
+        $('input[name="baopai"]').eq(i).val(baopai[i]);
+    }
+}
+
+function submit(ev) {
+
+    ev.preventDefault();
+
+    let paistr = $('[name="paistr"]').val();
+    if (! paistr) {
+        return false;
+    }
+    let shoupai = Majiang.Shoupai.fromString(paistr);
+
+    let param = Majiang.Util.hule_param();
+
+    let hule = Majiang.Util.hule(shoupai, null, param) || {};
+
+    let paipu = {
+        shoupai: paistr,
+        fu:      hule.fu,
+        fanshu:  hule.fanshu,
+        defen:   hule.defen,
+        hupai:   hule.hupai,
+    };
+
+    dialog.hule(paipu);
+
     return false;
 }
 
 $(function(){
+
+    dialog = new Majiang.UI.HuleDialog(
+                        $('.dialog'), Majiang.UI.pai('#loaddata'));
+
     $('form').on('submit', submit);
+    $('form').on('reset', function(){
+        hide($('.dialog'));
+        $('form input[name="paistr"]').focus();
+    });
+
+    init();
 });
