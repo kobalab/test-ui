c9367ac21b320ba4a4afb68f6bce0d17599b6c8a
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-04-05 00:17:58 +0800

    操作ガイド表示を追加

--- src/js/dev/player.js
+++ src/js/dev/player.js
@@ -4,6 +4,7 @@
 "use strict";
 
 const Majiang = { UI: require('../lib/') };
+const $ = require('jquery');
 
 module.exports = class Player extends Majiang.UI.Player {
 
@@ -15,11 +16,16 @@ module.exports = class Player extends Majiang.UI.Player {
 
     action(msg, callback) {
         if (this._auto_replay) {
+            $('#debug').hide();
             super.action(msg);
             if (callback) callback(this._reply.shift());
         }
         else {
-            if (callback) console.log(this._reply.shift());
+            if (callback) {
+                let reply = JSON.stringify(this._reply.shift());
+                if (reply == '{}') reply = '';
+                $('#debug').text(reply).show();
+            }
             super.action(msg, callback);
         }
     }
