c646097403adc9e4ec70d1bec52be705bd17443c
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-04-08 20:44:45 +0800

    v0.5.0 / 2026-04-08
      - 「ルール設定」を追加

--- src/js/majiang.js
+++ src/js/majiang.js
@@ -1,5 +1,5 @@
 /*!
- *  電脳麻将 v0.4.1
+ *  電脳麻将 v0.5.0
  *
  *  Copyright(C) 2017 Satoshi Kobayashi
  *  Released under the MIT license
@@ -9,7 +9,7 @@
 global.Majiang = require('@kobalab/majiang-core');
 global.Majiang.AI = require('@kobalab/majiang-ai');
 global.Majiang.UI = require('./lib/');
-global.Majiang.VERSION = '0.4.1';
+global.Majiang.VERSION = '0.5.0';
 global.jQuery = require('jquery');
 global.$ = jQuery;
 
