d961d50e7b5940de14be61f17bcb737af9e7ad93
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-04-11 06:09:22 +0800

    ボタンが有効なときだけ role="button" とし selector の制御下に置くよう修正

--- src/js/lib/gamectl.js
+++ src/js/lib/gamectl.js
@@ -15,7 +15,6 @@ module.exports = class GameCtl {
         this._views   = views;
 
         hide($('> *:not(.speed)', root));
-        $('.sound, .minus, .plus', root).attr('role','button');
 
         this._pref = localStorage.getItem(storage)
                         ? JSON.parse(localStorage.getItem(storage))
@@ -31,27 +30,35 @@ module.exports = class GameCtl {
 
         this.clear_handler();
 
-        $('.sound', this._root).on('click.controller', ()=>
+        $('.sound', this._root).attr('role','button')
+                               .on('click.controller', ()=>
                                     this.sound(! this._pref.sound_on));
-        $('.minus', this._root).on('click.controller', ()=>
-                                    this.speed(this._pref.speed - 1));
-        $('.plus',  this._root).on('click.controller', ()=>
-                                    this.speed(this._pref.speed + 1));
+        if (this._game) {
+            $('.minus', this._root).attr('role','button')
+                                   .on('click.controller', ()=>
+                                        this.speed(this._pref.speed - 1));
+            $('.plus',  this._root).attr('role','button')
+                                   .on('click.controller', ()=>
+                                        this.speed(this._pref.speed + 1));
+        }
 
         $(window).on('keyup.controler', (ev)=>{
-            if      (ev.key == 'a') return this.sound(! this._pref.sound_on);
-            else if (ev.key == '-') return this.speed(this._pref.speed - 1);
-            else if (ev.key == '+') return this.speed(this._pref.speed + 1);
+            if (ev.key == 'a') return this.sound(! this._pref.sound_on);
+            if (this._game) {
+                if      (ev.key == '-') return this.speed(this._pref.speed - 1);
+                else if (ev.key == '+') return this.speed(this._pref.speed + 1);
+            }
         });
 
-        setSelector($('.sound, .minus, .plus', this._root), '.controller',
+        setSelector($('[role="button"]', this._root), '.controller',
                         { prev: null, next: null, focus: null,
                           touch: false, hold: true });
     }
 
     clear_handler() {
         clearSelector('.controller');
-        $('.sound, .minus, .plus', this._root).off('click.controller');
+        $('[role="button"]', this._root).removeAttr('role')
+                                        .off('click.controller');
         $(window).off('.controller')
     }
 
