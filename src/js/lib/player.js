a51e37f9f5a81feba9ad373d33a586a330e1446c
Satoshi Kobayashi <koba@yk.rim.or.jp>
2026-04-10 23:51:00 +0800

    ネット対戦の場合は開局で待つよう修正

--- src/js/lib/player.js
+++ src/js/lib/player.js
@@ -31,8 +31,10 @@ module.exports = class Player extends Majiang.Player {
         this.clear_action();
         this.clear_mianzi();
         this.clear_dapai();
-        $('.dialog, .summary', this._root).off('click');
-        clearSelector('dailog');
+        $('.kaiju, .dialog, .summary', this._root).off('click');
+        clearSelector('kaiju');
+        clearSelector('dialog');
+        clearSelector('summary');
     }
 
     add_action(type, callback) {
@@ -117,7 +119,14 @@ module.exports = class Player extends Majiang.Player {
         clearSelector('dapai');
     }
 
-    action_kaiju(kaiju) { this.callback() }
+    action_kaiju(kaiju) {
+        if (! this._view) return this.callback();
+        setTimeout(()=>{
+            setSelector($('.kaiju .submit', this._root), 'kaiju',
+                        { prev: null, next: null });
+            $('.kaiju', this._root).on('click', ()=> this.callback());
+        }, 500);
+    }
 
     action_qipai(qipai) { this.callback() }
 
